b10c272626bdd839bebe7a0cdd23da47375e933c
JRobsonJr <jrobsonjr16@gmail.com>
2024-07-13 12:40:00 -0300

    Fix badges and box shadow crop

--- components/ui/PetCard.tsx
+++ components/ui/PetCard.tsx
@@ -1,5 +1,11 @@
-const Badge = ({ text, palette }: { text: string, palette: string }) => (
-    <span class={`whitespace-nowrap inline-flex items-center rounded-3xl px-3.5 py-1.5 text-base font-bold text-${palette} ring-2 ring-inset ring-${palette}`}>
+const LocationBadge = ({ text }: { text: string }) => (
+    <span class="whitespace-nowrap inline-flex items-center rounded-3xl px-3.5 py-1.5 text-base font-bold text-[#9747FF] ring-2 ring-inset ring-[#9747FF]">
+        {text}
+    </span>
+);
+
+const AgeBadge = ({ text }: { text: string }) => (
+    <span class="whitespace-nowrap inline-flex items-center rounded-3xl px-3.5 py-1.5 text-base font-bold text-[#FFA23B] ring-2 ring-inset ring-[#FFA23B]">
         {text}
     </span>
 );
@@ -17,14 +23,14 @@ interface Props {
 }
 
 const PetCard = ({ name = 'Neninho', imageUrl = 'https://placehold.co/300x400', location = 'Benvenuto', age = '2 years' }: Props) => (
-    <div class="flex w-full ml-4 space-y-3.5 flex-col w-full p-5 rounded-lg border-2 shadow-[-4px_4px_0px_rgba(0,0,0)] border-black">
+    <div class="flex w-full ml-4 mb-2 space-y-3.5 flex-col w-full p-5 rounded-lg border-2 shadow-[-4px_4px_0px_rgba(0,0,0)] border-black">
         <image class="w-full" src={imageUrl} />
         <p class="text-3xl font-bold">
             {name}
         </p>
         <div class="flex space-x-2">
-          <Badge palette="purple-700" text={location} />
-          <Badge palette="orange-500" text={age} />
+          <LocationBadge text={location} />
+          <AgeBadge text={age} />
         </div>
     </div>
 );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,16 +8,10 @@ export default {
     container: { center: true },
     extend: {
       animation: {
-        sliding: "sliding 10s linear infinite",
-        slidingDelay: "sliding 10s linear 5s infinite",
         scroll: "scroll 10s forwards linear infinite",
         scrollHover: "scroll 10s forwards linear paused infinite"
       },
       keyframes: {
-        sliding: {
-          "0%": { left: "100%" },
-          "100%": { left: "-100%" },
-        },
         scroll: {
           to: {
             transform: 'translate(calc(-50% - 0.5rem))'
