3eb1b1863206d7a8f9627a52e835475cef406225
Carlos Ribeiro <carlos.ribeiro@ccc.ufcg.edu.br>
2024-07-14 07:21:10 -0300

    update: Video.tsx

--- sections/Video.tsx
+++ sections/Video.tsx
@@ -1,19 +1,19 @@
-import type { ImageWidget } from "apps/admin/widgets.ts";
 import { VideoWidget } from "apps/admin/widgets.ts";
 
 export interface Props {
-  background: ImageWidget;
   video: VideoWidget;
 }
   
-export default function Video({ video, background }: Props) {
+export default function Video({ video }: Props) {
   return (
-    <div class="flex items-center justify-center bg-contain bg-no-repeat bg-center h-screen bg-[#636FFF]" style={{ backgroundImage: `url(${background})` }}>
-        <div class="flex rotate-[-1deg] pt-32 size-8/12 justify-center">
-          <video class="h-full" controls>
-            <source src={video}></source>
-          </video>
+    <div class="flex items-center justify-center bg-contain bg-no-repeat bg-center h-screen bg-[#636FFF]">
+        <div class="flex w-full max-w-[700px] rotate-[-1deg] justify-center bg-black rounded-xl"> 
+          <div class="flex w-full max-w-[700px] justify-center pb-3 pl-3">
+            <video class="w-full h-full rounded-xl border border-black" controls>
+              <source src={video}></source>
+            </video>
+          </div>
         </div>
     </div>
-);
+  );
 }
