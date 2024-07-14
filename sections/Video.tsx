8571ecb32e9277acebe7152a366312d62022043c
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-07-14 10:30:04 -0300

    Refs videos

--- sections/Video.tsx
+++ sections/Video.tsx
@@ -1,19 +1,18 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import { VideoWidget } from "apps/admin/widgets.ts";
+import Icon from "site/components/ui/Icon.tsx";
 
 export interface Props {
   title?: string;
-  doodle: ImageWidget;
   video: VideoWidget;
 }
   
-export default function Video({ title, video, doodle }: Props) {
+export default function Video({ title, video }: Props) {
   return (
-    <div class="flex flex-col items-center justify-center bg-contain bg-no-repeat bg-center h-screen bg-[#636FFF]">
-        <div class="flex items-center flex-grow">
+    <div class="group relative flex flex-col gap-8 overflow-hidden items-center justify-center bg-contain bg-no-repeat bg-center py-16 px-4 bg-secondary">
+        <div class="flex items-center">
           <p class="font-bold text-4xl text-white">{title}</p>
         </div>
-        
 
         <div class="flex w-full max-w-[700px] rotate-[-1deg] justify-center bg-black rounded-xl"> 
           <div class="flex w-full max-w-[700px] justify-center pb-3 pl-3">
@@ -23,9 +22,12 @@ export default function Video({ title, video, doodle }: Props) {
           </div>
         </div>
 
-        <div class="flex flex-grow w-full items-end">
-          <img src={doodle} alt="kitten doodle" class="object-contain max-w-[300px]"/>
-        </div>
+        <Icon
+        class="hidden md:block absolute rotate-180 translate-y-[16px] group-hover:translate-y-0 bottom-0 left-0 transition duration-300 ease-in-out"
+        id="glasses-cat"
+        height={40}
+        width={116}
+      />
     </div>
   );
 }
